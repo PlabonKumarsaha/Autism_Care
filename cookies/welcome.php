8b657a426ec81a78f34087a7a7b3082f83cca9a3
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-05-21 20:57:43 +0600

    Add files via upload

---
+++ cookies/welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Hello </h1>
+<?php
+//it will be shown for 1000 second.Then it will terminate
+
+//if we use session then the  data is removed afer some times or when the browser is closed
+echo $_COOKIE["Username"];
+
+?>
+
+</body>
+</html>
